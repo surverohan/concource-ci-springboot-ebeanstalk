c1513f5b29f5dc28624aa5b590293d44f93f5bd6
rohansurve rohansurve <rohansurve26@gmail.com>
2020-03-01 21:52:18 -0600

    terraform

--- tasks/terraform/terraform.sh
+++ tasks/terraform/terraform.sh
@@ -19,29 +19,12 @@ RED='\033[0;31m'
 export TF_IN_AUTOMATION="true"
 errors=0
 
-    echo "****************** BUILDING $env ******************"
-	echo " pwd -" 
-	pwd
-	ls
+    echo "****************** BUILDING  ******************"
 	cd  ci-pipeline/tasks/terraform
-		echo " pwd -"
-	pwd
-	ls
-  ## CP -R ci-pipeline/tasks/terraform .
-  ##  cd terraform-plan-out/dev
-
-    echo "BUILDING $env: terraform init"
+    echo "BUILDING terraform init..."
     terraform init
-    echo "BUILDING $env: terraform plan"
+    echo "BUILDING  terraform plan......."
 	terraform plan
-	echo "BUILDING $env: terraform plan done ....."
-
-    echo "BUILDING $env: terraform apply"
+    echo "BUILDING  terraform apply..............."
 	terraform apply -auto-approve
-	echo "BUILDING $env: terraform apply done ....."
-
-    ## touch tfplan.txt
-    ## set +e
-    ## terraform apply > tfplan.txt
-    ## planexit=$?
-    ## set -e
+	echo "BUILDING  terraform apply done !!!!!!!!!!!!!!!!!!!!!!!!"
